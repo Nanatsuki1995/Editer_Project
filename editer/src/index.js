5f03e4199847d0346876ca378b37aa63192be7d3
Nanatsuki1995 <514948303@qq.com>
2018-12-04 20:29:55 +0800

    项目第一天

---
+++ editer/src/index.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: zengjian 
+ * @Date: 2018-12-03 20:10:00 
+ * @Last Modified by: zengjian
+ * @Last Modified time: 2018-12-04 20:16:01
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import reducer from './reducer/index'
+import createStore from './store/store'
+import Provider from './redux/Provider'
+import middlewares from './middleware'
+import logger from './middleware/logger'
+
+const store=middlewares(new createStore(reducer,{}),[logger])
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>
+    , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
