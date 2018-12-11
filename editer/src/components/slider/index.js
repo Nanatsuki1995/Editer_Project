83edcc699471ca1980873cb7ec85a5b56024a8d5
Nanatsuki1995 <514948303@qq.com>
2018-12-11 20:35:15 +0800

    update

--- editer/src/components/slider/index.js
+++ editer/src/components/slider/index.js
@@ -2,7 +2,7 @@
  * @Author: zengjian 
  * @Date: 2018-12-10 20:00:56 
  * @Last Modified by: zengjian
- * @Last Modified time: 2018-12-10 20:06:18
+ * @Last Modified time: 2018-12-11 19:59:10
  */
 import React from 'react'
 import {Slider} from 'antd'
@@ -22,7 +22,7 @@ class SliderCmp extends React.Component{
     render(){
         return <Slider
             onChange={this.onChange}
-            min={0}
+            min={50}
             step={10}
             max={200}
             value={this.props.zoom}
